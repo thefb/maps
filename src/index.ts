403ba563cc4a36500bf56e0d62b2273ab1f081ca
Fabiano Siqueira <fabiano.siqueirab@gmail.com>
2021-09-17 00:56:04 -0300

    include map

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,12 @@ import { Company } from './Company';
 import { User } from './User';
 
 const user = new User()
-console.log(user)
 
-const company = new Company()
-console.log(company)
+const map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 6,
+  center: {
+    lat: user.location.lat,
+    lng: user.location.lng,
+  }
+})
+
