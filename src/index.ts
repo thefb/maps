2ed5a023998f741e43fade4b64549983ac89e906
Fabiano Siqueira <fabiano.siqueirab@gmail.com>
2021-09-17 00:32:38 -0300

    add company class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('hi there!')
+import { Company } from './Company';
+import { User } from './User';
+
+const user = new User()
+console.log(user)
+
+const company = new Company()
+console.log(company)
